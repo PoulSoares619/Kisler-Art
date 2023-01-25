bad975e1394094dc7fc76a10c63d57a402ff1e9e
Poul <poul.pheez2@gmail.com>
2023-01-25 16:24:58 -0300

    .

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
 const abrirMenu = () =>{
-    alert('ta funcionando')
+    document.getElementById('nav_open').style.width = "250px";
+    document.getElementById('nav_open').style.boxShadow = "0px 10px 100px 1600px rgba(0, 0, 0, 0.60)";
+    
+}
+const fechaMenu = () =>{
+    document.getElementById('nav_open').style.width = "0px";
+    document.getElementById('nav_open').style.boxShadow = 'none';
 }
