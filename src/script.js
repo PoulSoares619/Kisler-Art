d28658529e4495c862da00d16621c261e3f8c41c
Poul <poul.pheez2@gmail.com>
2023-02-07 21:42:19 -0300

    .

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
+var imagens = document.querySelectorAll('.small_img');
+console.log(imagens)
+var modal = document.querySelector('.modal');
+var modalImg = document.querySelector('#modal_img');
+var btClose = document.querySelector('#bt_close');
+var srcVal = '';
+
+for(var i = 0; i < imagens.length; i++){
+    imagens[i].addEventListener('click', () => {
+        srcVal = imagens[i].getAttribute('src');
+        modalImg.setAttribute('src', srcVal);
+        modal.classList.toggle('modal_active')//deixa o modal visivel no css
+    });
+    
+}
+
 const abrirMenu = () =>{
     document.getElementById('nav_open').style.width = "250px";
     document.getElementById('sombra-fundo').style.position = "fixed"
@@ -8,3 +24,11 @@ const fechaMenu = () =>{
     //document.getElementById('sombra-fundo').style.display = "hidden"
     document.getElementById('sombra-fundo').classList.add('hidden')
 }
+
+
+
+/*imagens.forEach((imagem)=>{
+    imagem.addEventListener('click',() => {
+        alert(imagem);
+    })
+})*/
